9429f40f3aba1d827144949a7756cc93ec4bfdcf
Tyler Lawson <tylerlawson12@live.com>
2019-10-07 12:36:46 -0700

    added sales.html and app.js with code

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 var parentEl = document.getElementById('parentElement');
 
-parentEl.textContent = 'attempting to contact html.' 
+parentEl.textContent = 'attempting to contact html.';
+var child = document.createElement('h1');
+child.textContent = 'salmon cookie shops';
+parentEl.appendChild(child);
+
+var iceCreamShop = {
+  shopHours: ['6am', '7am', '8am', 'so on'],
+  hoursTotal: [12],
+
+  locationName: 'Pikes Place Market',
+
+  render: function () {
+    for (var i = 0; i < this.shopHours.length; i++) {
+      var childEl = document.createElement('li');
+      childEl.textContent = `Shop Hours: ${this.shopHours[i]}~
+    Total Customers: ${this.hoursTotal[i]}`;
+      parentEl.appendChild(childEl);
+    }
+  }
+};
+
+iceCreamShop.render();
+
+
