bbbbcd9dc9b7372d79263faf9cbe2fd04ea40942
Tyler Lawson <tylerlawson12@live.com>
2019-10-07 16:11:31 -0700

    Putting hours, average customers, and finishing up 1st problem

--- app.js
+++ app.js
@@ -1,26 +1,92 @@
 var parentEl = document.getElementById('parentElement');
+// parentEl.textContent = 'attempting to contact html.';
 
-parentEl.textContent = 'attempting to contact html.';
 var child = document.createElement('h1');
-child.textContent = 'salmon cookie shops';
+child.textContent = 'Seattle: ';
 parentEl.appendChild(child);
 
-var iceCreamShop = {
-  shopHours: ['6am', '7am', '8am', 'so on'],
-  hoursTotal: [12],
+var seattle = {
+  shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'total'],
+  hourTotals: [],
+  locationName: 'Salmon Cookies',
+  minCust: 23,
+  maxCust: 65,
+  avCookie: 6.3,
 
-  locationName: 'Pikes Place Market',
+  getRandomInt: function (minCust, maxCust) {
+    minCust = Math.ceil(minCust);
+    maxCust = Math.floor(maxCust);
+    return Math.floor(Math.random() * (maxCust - minCust)) + minCust; //The maximum is exclusive and the minimum is inclusive
+  },
+
+  customerHour: function () {
+    for (var i = 0; i < this.shopHours.length; i++) {
+      var randomCustomerCount = this.getRandomInt(this.minCust, this.maxCust);
+      // console.log(randomCustomerCount);
+      this.hourTotals.push(randomCustomerCount);
+    }
+  },
+
+  getTotalCookies: function()
+
 
   render: function () {
+    this.customerHour();
     for (var i = 0; i < this.shopHours.length; i++) {
       var childEl = document.createElement('li');
-      childEl.textContent = `Shop Hours: ${this.shopHours[i]}~
-    Total Customers: ${this.hoursTotal[i]}`;
+      // childEl.textContent = 'Shop Hours: ' + this.shopHours[i];
+      var avCookie = Math.ceil(this.avCookie * this.hourTotals[i]);
+      childEl.textContent = `Hours: ${this.shopHours[i]} ~Customers: ${this.hourTotals[i]} ~Average Cookies: ${avCookie}`;
       parentEl.appendChild(childEl);
     }
   }
 };
 
-iceCreamShop.render();
+seattle.render();
+console.log(seattle);
+
+seattle.getRandomInt(23, 65);
+
+// seattle.customerHour();
+
+// console.log(seattle.getRandomInt(23, 65));
+// 'didnt work'
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var parentEl = document.getElementById('parentElement');
+
+// var child = document.createElement('h1');
+// child.textContent = 'Tokyo: ';
+// parentEl.appendChild(child);
+
+// var cookieShop = {
+//   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+//   hourTotals: [
+
+//   ],
+//   locationName: 'Get My Coffee',
+//   randomNumberGenerator: function () {
+
+//   },
+//   render: function () {
+//     for (var i = 0; i < this.shopHours.length; i++)
+//     var childEl = ducument.createElement('li');
+//    childEl.textContent
+//   }
+// }
