98a99811430e6ee735dd8b0decef1d5c1791e005
Tyler Lawson <tylerlawson12@live.com>
2019-10-09 14:56:10 -0700

    Pushing to github so I can create a new branch instead of working on master

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# ReadMe
+
+# Forms
+Form controls: Adding Text, Making Choices, Submitting Forms.
+
+### Forms
+- controls live inside a < form> element. carries the action attribute.
+
+### Action
+- every element requires and action attribute because its value is the url for the page on the server.
+
+### Methods
+ - Short forms, retrieving data from the web server, allows users to upload a file, contains sensitive data(passwords). Adds information to,  or deletes information from, a database.
+
+
+
+# Tables & Forms
+
+Table cells can have different borders and spacing in different browsers
+
+Forms are easier to use if the form controls are verticall aligned using CSS
+
+List markers can be given different appearances using the list-style-type and list-style image
+# Events
+
+* Events are the browsers way of indicating when something has happened.
+
+* The most commmonly used events are W3C DOM events, although there are others in the HTML5.
--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 
 var shopHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
+var allStores = [];
 
 function getRandomInt(minCust, maxCust) {
   return Math.floor(Math.random() * (maxCust - minCust)) + minCust;
 } //The maximum is exclusive and the minimum is inclusive
-var test = getRandomInt(2, 10);
+// var test = getRandomInt(2, 10);
 
-console.log(test);
+// console.log(test);
+
 Store.prototype.customerPerHour = function () {
   for (var i = 0; i < shopHours.length; i++) {
     var cookiesSoldPerHour = Math.ceil((getRandomInt(this.minimumCustomers, this.maximumCustomers)) * this.averageCookies);
-    console.log(cookiesSoldPerHour);
+    // console.log(cookiesSoldPerHour);
     this.hourTotals.push(cookiesSoldPerHour);
   }
 };
@@ -27,6 +29,7 @@ function Store(locationName, minimumCustomers, maximumCustomers, averageCookies)
   this.customerPerHour();
   this.totalCookiesSold();
   this.storeData(this);
+  allStores.push(this);
   // getRandomInt
 }
 
@@ -80,16 +83,46 @@ this.getRandomInt = function (minCust, maxCust) {
   return Math.floor(Math.random() * (maxCust - minCust)) + minCust;
 }; //The maximum is exclusive and the minimum is inclusive
 
+var footerRow = function () {
+  var row = document.createElement('tr');
+  var tdata = document.createElement('td');
+  row.textContent = 'Totals:';
+  row.appendChild(tdata);
 
+  for (var i = 0; i < shopHours.length; i++) {
+    var hourTotals = 0;
+    var tdataEl = document.createElement('td');
+    for (var j = 0; j < allStores.length; j++) {
+      hourTotals += allStores[j].hourTotals[i];
+      // console.log('ALL STORES x HOUR TOTALS:', allStores[j].hourTotals[i]);
+    }
+
+    tdataEl.textContent = hourTotals;
+    row.appendChild(tdataEl);
+  }
+  // var tdataE = document.createElement('td');
+  // row.appendChild(tdataE);
+  table.appendChild(row);
+};
+
+
+
+
+// eslint-disable-next-line no-unused-vars
 var seattleStore = new Store('Seattle', 23, 65, 6.3);
-console.log(seattleStore);
+// console.log(seattleStore);
+// eslint-disable-next-line no-unused-vars
 var tokyoStore = new Store('Tokyo', 3, 24, 1.2);
-console.log(tokyoStore);
-// var tokyoStore = new Store('Tokyo')
-// var dubaiStore = new Store('Dubai',)
+// console.log(tokyoStore);
+// eslint-disable-next-line no-unused-vars
+var dubaiStore = new Store('Dubai', 11, 38, 2.3);
+
+// eslint-disable-next-line no-unused-vars
+var parisStore = new Store('Paris', 20, 38, 2.3);
+
+// console.log("PARIS HOURS TOTAL",parisStore.hourTotals);
 
 
+console.log('allStores:', allStores[0].hourTotals[0]);
 
-// var test = function(min,max)
-// console.log(test) this is the random number function line 23
-// 9 times out of 10 undefined, means you havent returned function.
+footerRow();
